6ed398df06b681ff0c5e33d0e0ceab2619074244
Melanie Williams <melaniep518@gmail.com>
2016-12-20 12:19:06 -0500

    more tests

--- test/my_test.js
+++ test/my_test.js
@@ -1,7 +1,17 @@
 var expect = require('chai').expect;
 
+function myName() {
+	return 'melanie';
+}
+
 describe('my test suite', function () {
   it('fails majestically', function () {
-    expect(3).to.eql(2);
+    expect(5%4).to.eql(2);
+  });
+  it('will fail again', function () {
+    expect('my name').to.eql('melanie');
+  });
+  it('will finally pass', function () {
+    expect(myName()).to.eql('melanie');
   });
 });
