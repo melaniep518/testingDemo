89a763aec92b8ef7ea116945ee187f0894556616
Melanie Williams <melaniep518@gmail.com>
2016-12-20 12:41:23 -0500

    more tests

--- test/my_test.js
+++ test/my_test.js
@@ -4,6 +4,15 @@ function myName() {
 	return 'melanie';
 }
 
+function add(num1, num2) {
+	return num1 + num2;
+}
+
+
+// describe is a series of tests
+// it statement tests individual functions
+// expect statement: first argument is what we are testing, second argument is what we are expecting to recieve
+
 describe('my test suite', function () {
   it('fails majestically', function () {
     expect(5%4).to.eql(2);
@@ -13,5 +22,13 @@ describe('my test suite', function () {
   });
   it('will finally pass', function () {
     expect(myName()).to.eql('melanie');
+  }); 
+  it('will pass again', function () {
+    expect(add(3,4)).to.eql(7);
+    expect(add(3,4)).be.a('number');
+  });
+  it('will probably fail', function () {
+    expect(add(3,4)).to.eql(7);
+    expect(add(3,4)).be.a('string');
   });
 });
